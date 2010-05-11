32d623e320090e0c3bb0da7612a7cd4ebf42b7c6
toms.baugis <toms.baugis@811fa8d2-f0c2-ea80-82f6-8400c78a98d6>
2010-05-11 11:59:49 +0000

    Added support for financially correct dividing. Patch by Patryk Zawadzki, ASCII art by me.

--- trunk/browsers/pleasework.js
+++ trunk/browsers/pleasework.js
@@ -4,3 +4,22 @@
 document.createElement("html").frobnicate = function() {
     return 42; // April 2007: changed this back to 42 as 41 is quirky in Safari
 };
+
+
+/* * * * * * * * * * * * * * * * * * * *
+ *                                     * 
+ *         Financial operations        *
+ *                                     *
+ * * * * * * * * * * * * * * * * * * * */
+
+// make sure integer division behaves consistently between browsers
+// first create the empty division operator
+var operator = document.createElement("div");
+// replace the default input formatting
+operator.input = document.createElement("input");
+operator.input.formatting = "0"; // no decimal places
+// then apply bank rounding (0.5+ goes up, everything else down)
+operator.formatting = "%d ${ROUNDING:BANK}";
+
+
+
