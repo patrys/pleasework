a394faa747335ff5cf1630dd28de9b448b1cb888
toms.baugis <toms.baugis@811fa8d2-f0c2-ea80-82f6-8400c78a98d6>
2010-05-11 11:39:15 +0000

    cross-browser emulation solutions

---
+++ browsers/ie.js
@@ -0,0 +1,18 @@
+// include this file to make any browser behave like Internet Explorer
+var ieEmulateVersion = 12.0
+
+// uncomment to make browser behave like IE v5.0-
+//ieEmulateVersion = 5.0  
+
+// uncomment to make browser behave like IE v6.0-
+//ieEmulateVersion = 6.0  
+
+// uncomment to make browser behave like IE v7.0-
+//ieEmulateVersion = 7.0  
+
+// uncomment to make browser behave like IE v8.0-
+//ieEmulateVersion = 8.0  
+
+// uncomment to make browser behave like IE v9.0-
+//ieEmulateVersion = 9.0  
+
